eb22a3ac7645bb6fa13788aae3d83cf97c303605
Melnichuk Michael <mikel.melnichuk@gmail.com>
2026-05-05 17:01:46 +0300

    BugFix - use string

--- internal/models/event.go
+++ internal/models/event.go
@@ -1,11 +1,9 @@
 package models
 
-import "time"
-
 type Event struct {
-	ID    string    `json:"id"`
-	Title string    `json:"title"`
-	Start time.Time `json:"start"`
-	End   time.Time `json:"end"`
-	Day   string    `json:"day"` // YYYY-MM-DD format (e.g., "2026-03-18")
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Start string `json:"start"`
+	End   string `json:"end"`
+	Day   string `json:"day"` // YYYY-MM-DD format (e.g., "2026-03-18")
 }
