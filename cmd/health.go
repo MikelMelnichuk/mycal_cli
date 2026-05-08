80c5625c6b6192c29977a8ba2a25efd7f6ee6ecc
Melnichuk Michael <mikel.melnichuk@gmail.com>
2026-05-08 22:51:41 +0300

    Implement the test/health command

---
+++ cmd/health.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// mycal test
+// mycal test --verbose
+
+var testCMD = &cobra.Command{
+	Use:     "test",
+	Aliases: []string{"health"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var verbose, _ = cmd.Flags().GetBool("verbose")
+
+		// If verbose check of the backend connection
+		if verbose {
+			var err = APIClient.HealthCheckBackend()
+			if err != nil {
+				return err
+			}
+		}
+
+		// In any case check of connection for the backend and DB
+		var err = APIClient.HealthCheckDB()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	testCMD.Flags().Bool("verbose", false, "Enable verbose mode, for more logs")
+	rootCmd.AddCommand(testCMD)
+}
